6a60568419ab2dc525ab702b0e6213d109a0c801
Kiriti K <kiriti.k999@gmail.com>
2018-09-17 02:03:16 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   stripeKey: 'pk_test_yL3DSrbEaoeHxTwRPmcZpxWj',
-  apiUrl: 'http://www.granddesigns.in'
+  apiUrl: 'https://www.granddesigns.in'
 };
