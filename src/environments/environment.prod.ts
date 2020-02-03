ca5b41b96f1ac8533be16a7723db2d3c7c0a38c7
Kiriti Komaragiri <Kiriti_Komaragiri@epam.com>
2020-02-03 14:32:29 +0530

    create prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   stripeKey: 'pk_test_yL3DSrbEaoeHxTwRPmcZpxWj',
-  apiUrl: 'https://www.granddesigns.in'
+  // apiUrl: 'https://www.granddesigns.in'
+  apiUrl: 'http://granddesigns.herokuapp.com/'
 };
