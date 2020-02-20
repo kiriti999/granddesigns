899d27d2126e192055204d5dfc0508868bd74fe6
Kiriti K <kiriti.k999@gmail.com>
2020-02-21 01:11:37 +0530

    validation

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,52 +6,50 @@ import { Router } from '@angular/router';
 import { WindowRef } from '../windowRef.service';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  btnDisabled = false;
+    btnDisabled = false;
 
-  quantities = [];
+    quantities = [];
 
-  constructor(
+    constructor(private data: DataService, private router: Router, private globalService: GlobalService) {}
 
-    private data: DataService,
-    private router: Router,
-    private globalService: GlobalService
-  ) { }
+    trackByCartItems(index: number, item: any) {
+        return item._id;
+    }
 
-  trackByCartItems(index: number, item: any) {
-    return item._id;
-  }
+    get cartItems() {
+        return this.data.getCart();
+    }
 
-  get cartItems() {
-    return this.data.getCart();
-  }
+    get cartTotal() {
+        let total = 0;
+        this.globalService.cartItems().forEach((data, index) => {
+            total += data['price'] * this.quantities[index];
+        });
+        return total;
+    }
 
-  get cartTotal() {
-    let total = 0;
-    this.globalService.cartItems().forEach((data, index) => {
-      total += data['price'] * this.quantities[index];
-    });
-    return total;
-  }
+    removeProduct(index, product) {
+        this.quantities.splice(index, 1);
+        this.data.removeFromCart(product);
+    }
 
-  removeProduct(index, product) {
-    this.quantities.splice(index, 1);
-    this.data.removeFromCart(product);
-  }
+    ngOnInit() {
+        this.globalService.cartItems().forEach(data => {
+            this.quantities.push(1);
+        });
+    }
 
-  ngOnInit() {
-    this.globalService.cartItems().forEach(data => {
-      this.quantities.push(1);
-    });
-  }
+    get token() {
+        return localStorage.getItem('token');
+    }
 
-  goToAddressPage() {
-    // tslint:disable-next-line:max-line-length
-    this.router.navigate(['/profile/address', { state: JSON.stringify({ checkoutButton: true, quantities: this.quantities }) }]);
-  }
-
+    goToAddressPage() {
+        // tslint:disable-next-line:max-line-length
+        this.router.navigate(['/profile/address', { state: JSON.stringify({ checkoutButton: true, quantities: this.quantities }) }]);
+    }
 }
